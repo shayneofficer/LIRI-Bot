d46bdd15e710fd74ae5c72bab1c3fb4dc1c463ca
shayneofficer <shayneofficer@gmail.com>
2018-12-06 16:09:47 -0600

    added the concert functionality plus inquirer prompt

--- liri.js
+++ liri.js
@@ -1,15 +1,42 @@
 require("dotenv").config();
-
+var inquirer = require("inquirer");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var moment = require("moment");
-
+var fs = require("fs");
 const keys = require("./keys");
 
 var spotify = new Spotify(keys.spotify);
 
-var command = process.argv[2];
-var title = process.argv[3];
+inquirer.prompt([
+    {
+        type: "list",
+        message: "Please choose a command",
+        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+        name: "commandChoice"
+    }
+]).then(function (ans) {
+    // Switch Statement Version: 
+    switch (ans.commandChoice) {
+        case "concert-this":
+            console.log("concert");
+            getConcerts();
+            break;
+        case "spotify-this-song":
+            console.log("song");
+            break;
+        case "movie-this":
+            console.log("movie");
+            break;
+        case "do-what-it-says":
+            console.log("who knows?");
+            break;
+
+        default:
+            console.log("bad input");
+            break;
+    }
+})
 
 // If/Else Version:
 // if (command === "concert-this") {
@@ -24,23 +51,20 @@ var title = process.argv[3];
 //     // Do nothing
 // }
 
+function getConcerts() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What band?",
+            name: "band"
+        }
+    ]).then(function (res) {
+        axios.get(`https://rest.bandsintown.com/artists/${res.band}/events?app_id=${keys.bands.id}`).then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                var concertDate = moment(response.data[i].datetime).format("MM/DD/YYYY");
 
-// Switch Statement Version: 
-switch (command) {
-    case "concert-this":
-        console.log("concert");
-        break;
-    case "spotify-this-song":
-        console.log("song");
-        break;
-    case "movie-this":
-        console.log("movie");
-        break;
-    case "do-what-it-says":
-        console.log("who knows?");
-        break;
-
-    default:
-        console.log("bad input");
-        break;
+                console.log(`${response.data[i].venue.name} || ${response.data[i].venue.city} || ${concertDate}`);
+            }
+        });
+    });
 }
